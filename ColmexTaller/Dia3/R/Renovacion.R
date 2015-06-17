c5909a5f824ab9bcde9dd65d69fcc858328fe493
Tim Riffe <tim.riffe@gmail.com>
2015-06-17 13:10:22 -0500

    cambios introducidos durante la clase de miercoles

--- ColmexTaller/Dia3/R/Renovacion.R
+++ ColmexTaller/Dia3/R/Renovacion.R
@@ -1,4 +1,6 @@
 
+Nac    <- dget("HFDSWE.txt")
+
 ##############################
 # vamos a hacer Lotka en R!
 ##############################
@@ -9,8 +11,8 @@ library(HMDHFDplus)
 library(LifeTable) # si no lo tienes, mira en el codigo de lunes
 ##############################
 # si hace falta, define username & password
-#us <- userInput()
-#pw <- userInput()
+us <- userInput()
+pw <- userInput()
 Pais <- "SWE"
 Def <- readHMDweb(Pais,"Deaths_1x1",us,pw)
 Exp <- readHMDweb(Pais,"Exposures_1x1",us,pw)
@@ -19,9 +21,6 @@ Exp <- readHMDweb(Pais,"Exposures_1x1",us,pw)
 #Nac <- readHFDweb(Pais,"birthsRR",us,pw)
 # plan B: cargar datos localmente (mira en carpeta /Dia3/datos/
 
-### CAMBIA ESTO a tu carpeta de Dia3!!! ############
-setwd("/home/tim/git/ColmexTaller/ColmexTaller/Dia3")
-Nac <- dget("datos/HFDSWE.txt")
 
 # los datos de HMD y HFD son de dimensiones distintas
 
@@ -30,7 +29,6 @@ range(Nac$Year)
 range(Def$Year)
 
 # y los nacimientos de HFD no estan clasificados por sexo:
-
 # y recuerde que queremos hijas nacidas a madres.
 # asi que tendremos que dividir los nacimientos segun algun suposito
 # lo normal es 1.05 ... 
@@ -66,8 +64,11 @@ Nx <- Nac$Births[Nac$Year == año][1:111]
 #Nx <- c(rep(0,12),Nx,rep(0,55))
 # calculamos tasas de fecundidad:
 fx <- Nx / Ex
+
 # limpieza, sino tenemos casos de NaN:
 fx[Ex == 0] <- 0
+
+#plot(0:110, fx, type = 'l')
 
 # vector de edades:
 edades <- 0:110
@@ -86,6 +87,7 @@ plot(edades, Tabla$lx, type = 'l', main = paste("supervivencia",Pais,sexo,año))
 ######################################################
 # R0 de Lotka:
 (R0 <- sum(Lx*fx))
+
 # interpretacion: es el ISF feminino con un descuento 
 # para la mortalidad de las madres. No garantiza que 
 # las hijas llegan a edades reproductivas, solo que nacen.
@@ -109,33 +111,203 @@ LotkaMin <- function(r, Lx, fx, x = .5:110.5){
                                    # quiere decir que 'r' esta bien.
 
 # ya con este r sabemos la estructura estable de la poblacion:
+
+plot(edades, exp(-(edades+.5)*r)*Lx , type = 'l')
 cx <- exp(-(edades+.5)*r)*Lx / sum(exp(-(edades+.5)*r)*Lx)
 plot(edades, cx, type = 'l')
 # compare con el Lx tal cual:
 lines(edades, Lx/sum(Lx), col = "blue")
 legend("topright", lty=1, col=c("black","blue"), legend = c("c(x) estable","l(x) escalada"))
 
+cx2 <- exp(-(edades+.5)*-r)*Lx / sum(exp(-(edades+.5)*-r)*Lx)
+lines(edades, cx2, col = "red")
+
 # duracion media de una generacion
 (TG <- log(R0) / r) # TG = tiempo de generacion
 
+R0
+
 # es un poco diferente que la edad promedia de fx:
 (Mbar <- sum((edades + .5) * fx) / sum(fx))
+(Abar <- sum((edades + .5) * fx * Lx) / sum(fx * Lx))
 # pero muy parecido!
 
 # esto es conveniente porque tal vez no hace falta optimizar r tan exactamente,
 # si tenemos una relacion entre r, R0, y TG
 log(R0) / TG
 log(R0) / Mbar # diferente en cuarto digito, no mal!
-
+log(R0) / Abar
 #####################################################################
 # ahora a repitir para poblaciones tanatologicas
 
 # hace falta una matriz de dx parecida a la de f(y|x),
 # pero no escalada:
+
+# Todo depende del dx!
 dx <- Tabla$dx
 N  <- length(dx)
+# importamos la funcion de fya del lunes
+da2fya <- function(da){
+	
+	N <- length(da)
+	# definimos una matriz vacia:
+	fya <- matrix(0,N,N)
+	# ahora iteramos para rellenarlo con da:
+	for (i in 1:N){
+		# busca la fila i
+		# despues ponemos el vector desde la columna 1 hasta su final
+		fya[i,1:(N-i+1)] <- da[i:N]
+	}
+	# lo unico que hace falta es que las filas suman a 1
+	# nota que rowSums(fya) = l(a) = colSums(fya) en este momento
+	fya <- fya / rowSums(fya)
+	# el ultimo objecto esta devuelto por defecto
+	fya
+}
+
+# el fya que usamos
+fya <- da2fya(dx)
+
+# se usa para reproportionar los eventos y los estocs clasificadas
+# por ead cronologica en edad tanatologica
+Ny  <- colSums(Nx * fya) # filas crono | columna tano
+Ey  <- colSums(Ex * fya)
+
+# calculamos las tasa de fecundidad tanatologicas
+fy  <- Ny / Ey
+
+# el ISF tanatatologica ''siempre'' es mas alta que el ISF normal
+sum(fy)
+# miramos
+plot(edades, fy, type = 'l')
+
+# ahora ponemos dx en una matriz no escalada
+dxy <- matrix(0,N,N)
+for (i in 1:N){
+	# filas crono | columnas tano
+	dxy[i, 1:(N-i+1)] <- dx[i:N]
+}
+
+# decimos que las filas son cronologicas y las columnas son tanatologicas
+sum(colSums(dxy * exp(-x*r)) * fy)
+exp(-x*r)*Lx
+x <- .5:110.5
+plot(edades, colSums(dxy * exp(-x*-r)) / sum(dxy * exp(-x*-r)), 
+		type = 'l', ylim = c(0,.025))
+lines(edades, exp(-x*-r)*Lx / sum(exp(-x*-r)*Lx), 
+		col = "blue")
+
+# ahora queremos optimizar el 'r' de la equacion de renovacion
+# tanatologica:
+
+ThanoMin <- function(rt,dxy,fy,x=.5:110.5){
+	abs(1 - sum(colSums(dxy * exp(-x*rt)) * fy))
+}
+
+rt <- optimize(ThanoMin, c(-.05,.05), dxy= dxy, fy=fy, tol = 1e-15)$minimum
+
+r ; rt
+sum(colSums(dxy * exp(-x*.011)) * fy)
+
+library(devtools)
+install_github("timriffe/Pyramid/Pyramid")
+
+library(Pyramid)
+
+args(Pyramid)
+
+Exh <- Exp[Exp$Year == año, "Male"]
+
+Pyramid(Exh, Ex, grid = FALSE,
+		fill.males="royalblue",fill.females="royalblue",
+		coh.axis = TRUE,year = 1900, prop = FALSE)
+segments(0,0,0,110,col = 'white')
+args(Pyramid)
+
+Dxh <- Def[Def$Year == año,"Male"]
+
+Dxh[Dxh == 0] <- .1
+Exh[Exh == 0] <- .1
+
+Tablah <- LT(Exh, Dxh, mxsmooth = FALSE)
+
+fyxh <- da2fya(Tablah$dx)
+fyxf <- da2fya(Tabla$dx)
+
+Pxyf <- Ex * fyxf
+Pxyh <- Exh * fyxh
+
+Pyramid(Pxyh, Pxyf, grid = FALSE,
+		coh.axis = TRUE,year = 1900, prop = FALSE, 
+		border.males = NA, border.females = NA
+		)
+segments(0,0,0,110,col = 'white')
+
+library(RColorBrewer)
+display.brewer.all()
+cols <- brewer.pal(9, "YlGn")
+colRampa <- colorRampPalette(cols, space = "Lab")
+colRampa(111)
+
+Pyramid(Pxyh, Pxyf, grid = FALSE,
+		coh.axis = TRUE,year = 1900, prop = FALSE, 
+		border.males = NA, border.females = NA, 
+		fill.males = colRampa(111), fill.females = colRampa(111)
+)
+
+aggVec <- function(x,Int){
+	ages <- 1:length(x) - 1
+	groups <- ages - ages %% Int
+	tapply(x,groups,sum, na.rm = TRUE)
+}
+aggVec(Ex,10)
+
+PxyhAgg <- t(apply(Pxyh,1,aggVec,Int = 10))
+PxyfAgg <- t(apply(Pxyf,1,aggVec,Int = 10))
+
+Pyramid(PxyhAgg, PxyfAgg, grid = FALSE,
+		coh.axis = TRUE,year = 1900, prop = TRUE, 
+		border.males = NA, border.females = NA, 
+		fill.males = colRampa(11), fill.females = colRampa(11)
+)
+
+PyramidOutline(Exh,Ex,scale = 100)
 
 
-ddx <- matrix(0,)
+
+
+PxyhAggT <- t(apply(Pxyh,2,aggVec,Int = 10))
+PxyfAggT <- t(apply(Pxyf,2,aggVec,Int = 10))
+
+Pyramid(PxyhAggT, PxyfAggT, grid = FALSE,
+		coh.axis = TRUE,year = 1900, prop = TRUE, 
+		border.males = NA, border.females = NA, 
+		fill.males = rev(colRampa(11)), fill.females = rev(colRampa(11))
+)
+
+PyramidOutline(rowSums(PxyhAggT),rowSums(PxyfAggT),scale = 100)
+
+head(Nac)
+Nac <- Nac[!is.na(Nac$Births), ]
+
+Nac$Cohort <- Nac$Year - Nac$Age
+
+library(reshape2)
+
+NacAP <- acast(Nac[Nac$Age >= 13 & Nac$Age <= 55, ], 
+		       Age~Year, value.var = "Births")
+NacAC <- acast(Nac[Nac$Age >= 13 & Nac$Age <= 55, ], 
+		Age~Cohort, value.var = "Births")
+nrow(NacAP)
+barplot(NacAP, border = NA, col = colRampa(43), space = 0)
+
+NacAPAgg <- apply(NacAP,2,aggVec,Int = 5)
+NacACAgg <- apply(NacAC,2,aggVec,Int = 5)
+nrow(NacAPAgg)
+dev.new()
+barplot(NacAPAgg, border = NA, col = colRampa(9), space = 0)
+barplot(NacACAgg, border = NA, col = colRampa(9), space = 0)
+
+
 
 
