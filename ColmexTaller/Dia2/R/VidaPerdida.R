c77cc9a6bdc0768e74888c191516f9f0912ce798
Tim Riffe <tim.riffe@gmail.com>
2015-06-16 19:13:41 -0500

    he hecho unas mejoras con el grafico al final. Mas iteraciones...

--- ColmexTaller/Dia2/R/VidaPerdida.R
+++ ColmexTaller/Dia2/R/VidaPerdida.R
@@ -101,7 +101,8 @@ hazmeLX <- function(mx){
 	LX
 }
 # compare sumas: uau son iguales!
-sum(Dx * hazmeLX(Mx)) ; sum(APPx)
+LX <- hazmeLX(Mx)
+sum(Dx * LX) ; sum(APPx)
 
 barplot(colSums(Dx * LX), main = "misma cantidad, pero redistribuida", border = NA, space = 0)
 
@@ -110,26 +111,129 @@ barplot(Dx * LX, main = "misma cantidad, pero redistribuida", border = NA, space
 
 PPX <- Dx * LX
 
+###############################################3
+# escrito en clase: queremos un grafico con mejor definicion:
+# agrupar edades. Hay muchas maneras, y esta es la que se me
+# ha ocurrido en clase, asi que anoto aqui los pasos
+# nuestras edades sencillas que queremos agrupar: estan en filas
 Sencillas <- row(PPX) - 1
+# el factor de agrupacion: es decir el intervalo de edad que queremos
 Fac <- 10
-
+# %% es el modulo, 
+# como cuando tenias 7 años y querias dividir 6/10 = 1 con 4 de resto
 Grup <- Sencillas - Sencillas %% Fac
+# un vector que usaremos en el bucle para seleccionar...
 unicas <- unique(Grup[,1])
- i <- 1
+# iniciamos ppxout como objeto vacio
  ppxout <- NULL
+ # una iteracion por cada grupo de edad
 for (i in 1:11){
-	
+	# una matriz hecha solo de los del grupo de edad
 	PPXi <- matrix(PPX[Grup == unicas[i]],ncol = ncol(PPX))
+	# aggregar cada columna: acabamos un un vector
 	ppxi <- colSums(PPXi)
+	# pegar fila nueva
 	ppxout <- rbind(ppxout, ppxi)
 }
 
 barplot(ppxout, main = "misma cantidad, pero redistribuida", border = NA, space = 0)
 
-MiRampa <- colorRampPalette(c("yellow","orange","red"), space = "Lab")
-MiRampa(11)
+## Ahora, si queremos unos colores mas impactantes, pero tambien
+## sin violar mejores practicas con el uso de color en areas, lo
+## mas facil siempre es referir al package RColorBrewer. Tambien
+## vale para mapas! mira http://colorbrewer2.org/ y otros sitios asi
+
+## 1) installa si hace falta
+##install.packages("RColorBrewer")
+library(RColorBrewer)
+
+# mira las paletas que tiene:
+# display.brewer.all()
+# queremos uno del primer grupo en este caso!
+BrewCol <- brewer.pal(9,"GnBu") # primer argumento = cuantos colores, segundo cual paleta
+# Problema: tenemos 11 clases de edad y solo 9 colores
+# solucion: interpolamos colores.
+# colorRampPalette() devuelve una funcion para tu paleta. Si le das mas colores la
+# interpolacion puede salir mejor. Luego elige uno del los 'espacios' de color.
+# RGB es el defecto... y es defectuoso como espacio de color....
+# Yo suelo usar Lab, pero existen otros.
+MiRampa <- colorRampPalette(BrewCol, space = "Lab")
+miscols <- MiRampa(11) # se usa de esta forma
 barplot(ppxout, main = "misma cantidad, pero redistribuida", 
-		border = NA, space = 0, col = MiRampa(11))
+		border = NA, space = 0, col = miscols)
+# o con los colores al reves:
+barplot(ppxout, main = "misma cantidad, pero redistribuida", 
+		border = NA, space = 0, col = rev(miscols))
+##################################################################
+# ahora los colores ligeros se pueden perder contra el blanco.
+# quiero ponerle una linea delgada de perfil para darle forma.
+# cambio de estrategia: barplot() nos ha hecho lo que puede,
+# pero cuando se quiere controlar bien los detalles hay que 
+# usar funciones mas primitivos a veces....
+##################################################################
+# Todo lo de abajo es lo que llamo artisanal. Implica trabajo!
+# Solo vale la pena hacerlo para presentaciones, entradas de blog
+# articulos, y tal vez tesis. Este tipo de trabajo primitivo NO
+# es recomendado si estas explorando dattos... para explorar, usa
+# base, lattice o ggplot2. Construimos de esta forma para que se ve
+# lo que hace falta para tener control total sobre una figura.
+##################################################################
+# 1) abrir plot() vacio pero con las dimensiones correctas:
+plot(NULL, type = "n", xlim = c(0,110), ylim = c(0, 170000), 
+		axes = FALSE, xlab = "", ylab = "")
+# 2) usare poligonos para las barras de color. No un poligon
+#    por barra vertical, como en barplot(), sino un poligono unico
+#    para cada region de color. Es mas complicado, pero sale mejor
+#    calidad siempre si lo guardas... 
+
+# estrategia: hacemos la suma cumulativa de cada columna de ppxout:
+# apply es un bucle interno | 
+# '2' dice que operamos sobre columnas | 
+# cumsum es la funcion que applicamos
+ppxcum <- apply(ppxout,2,cumsum)
+# 3) ahora, cada valor y en esa matriz tiene 2 valores x...
+# (porque estamos trabajando con valores discretos, y se usa
+# mejor barras en este caso...)
+x      <- c(0,rep(1:109,each=2),110)
+# 4) añadir fila de ceros en pppxcum, porque el fondo es plano:
+ppxcum <- rbind(0,ppxcum)
+# 5) listos para un bucle para dibjuar los poligonos!
+for ( i in 1:11){
+	# el poligono tiene un fondo y un techo! dibuja siempre
+	# en la misma direccion, por eso repitimos los x asi.
+	polygon(x = c(x,rev(x)), y = rep(c(ppxcum[i,],rev(ppxcum[i+1,])),each=2),
+			border = NA, col = rev(miscols)[i])
+}
+# 6) ya esta pintando mejor!
+# ahora ponemos lineas de referencias que no nos molesten!
+abline(v = seq(10,100,by=10), col = "#FFFFFF50", lwd = .5) # blaco, semitransparente :-)
+abline(h = seq(25000,150000,by = 25000),, col = "#FFFFFF50", lwd = .5)
+# ojo: siempre hay que andar con quidado con las lineas de referencia.
+# si los hace y piensas que distraen, quitelos! siempre!
+
+# 7) la linea de perfil
+lines(x,rep(ppxcum[nrow(ppxcum),],each=2))
+# 8) ahora ejes
+# segments es super basico.
+# 8.1) eje y:
+segments(0,0,0,150000)            
+segments(0,seq(0,150000,by = 25000), # xpd nos da permiso de dibujar fuera de la area
+		-1,seq(0,150000,by = 25000), xpd = TRUE)
+options(scipen=6) # para que no nos escribe 100000 en vez de 1e5 ...
+text(0,seq(0,150000,by = 25000),        # pos = 2 dice que alinea a la izquierda.
+		labels=seq(0,150000,by = 25000),pos=2,xpd=TRUE)
+# 8.2) eje x:
+segments(seq(0,110,by=10),0,seq(0,110,by=10),-2000,xpd=TRUE)
+text(seq(0,110,by=10),-1000,seq(0,110,by=10),pos=1,xpd=TRUE) # pos = 1 = abajo
+
+# Todavia hace falta etiquetas de eje, y una leyenda explicativa con color,
+# tendrias que luego quitar el espacio blanco de los margenes. El titulo
+# lo pones siempre en el paper y mejor no en el mismo grafico.
+
+# finalmente: gaurdalo como pdf, asi se queda en formato vector. Para Word or PPT,
+# se puede investigar como guardar en formato EMF (pero lastima, graficos en formato
+# vector de Windows no apoyan la transperencia, y lo hemos usado). NUNCA usa jpg
+# para este tipo de grafico. Lo dire mañana en clase.
 
 ##########################################################
 # paramos aqui: sugiero intentarlo con causes el viernes
